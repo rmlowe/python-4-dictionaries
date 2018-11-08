4633a7d2ace9cd1e7f1ba09a5ffc03eb2d6d1d69
Robert Lowe <robertmlowe@rmlowe.com>
2018-11-08 14:07:34 +0800

    Part 5 - Tuples, Sets, and Booleans

--- notes.py
+++ notes.py
@@ -1,6 +1,12 @@
-# Dictionaries
-my_stuff = {'lunch':'pizza','bfast':'eggs'}
-my_stuff['lunch'] = 'burger'
-my_stuff['dinner'] = 'pasta'
-print(my_stuff['lunch'])
-print(my_stuff)
+# Sets
+x = set()
+
+x.add(1)
+x.add(2)
+x.add(4)
+x.add(4)
+x.add(4)
+print(x)
+
+converted = set([1,1,1,1,2,2,2,2,2,3,3,3,3])
+print(converted)
